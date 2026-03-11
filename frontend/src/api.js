f867a6163512aa48e5afdc591c447f827119c741
HerShey-sys <harshithavankadara52@gmail.com>
2026-03-11 19:55:12 +0530

    Fix: Define isLocalhost in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+
 const API = axios.create({ 
   // ADD THE /api SUFFIX HERE
   baseURL: isLocalhost 
